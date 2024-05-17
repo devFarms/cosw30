787e9deff320d6eb0454c80513bb5678f6e9601f
Alex <alex@devfarms.com>
2024-05-16 18:18:21 -0700

    Update list_posts.php

--- week12/list_posts.php
+++ week12/list_posts.php
@@ -22,7 +22,7 @@ text-align:center;
 <?php 
 require('mysqli_connect.php'); // use require because we want to force this to exist before running our queries
 
-echo "<h1>List of Website Users</h1>";
+echo "<h1>List of Posts</h1>";
 //And now to perform a simple query to make sure it's working
 $query = "SELECT * FROM BLOG_POSTS_TBL";
 $result = mysqli_query($connection, $query);
